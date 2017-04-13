9d021864474178d68691d11a9f3b9f5c5f67c1fb
Khachik Sukiasyan <khachatur125@gmail.com>
2017-04-14 01:01:11 +0400

    added functionality

--- SignInWindow/SignInWindow/MainWindow.xaml.cs
+++ SignInWindow/SignInWindow/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace SignInWindow
 {
@@ -23,6 +24,33 @@ namespace SignInWindow
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(loginTextBox.Text) && IsValidMail(loginTextBox.Text) && !string.IsNullOrEmpty(passwordTextBox.Password))
+            {
+
+                string name = loginTextBox.Text.Split('@')[0];
+                MessageBox.Show(string.Format("Welcome {0} !", name) );
+            }
+            else
+                MessageBox.Show("Incorrect mail or password !");
+
+        }
+
+        private bool IsValidMail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
         }
     }
 }
